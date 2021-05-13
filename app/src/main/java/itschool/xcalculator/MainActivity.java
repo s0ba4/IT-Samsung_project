449d0c7843f5e1489ffeb14a4b83b0017b945163
s0ba4 <sobachippin@gmail.com>
2021-05-13 22:52:27 +0200

    strings & colors fix

--- app/src/main/java/itschool/xcalculator/MainActivity.java
+++ app/src/main/java/itschool/xcalculator/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.InputType;
+import android.widget.Button;
 import android.widget.EditText;
 
 import itschool.xcalculator.databinding.ActivityMainBinding;
@@ -23,24 +24,24 @@ public class MainActivity extends AppCompatActivity {
         binding.input.setShowSoftInputOnFocus(false);
         binding.functionContainer.setOnClickListener((v) -> {});
 
-        binding.digit0.setOnClickListener((v) -> insertSymbol("0"));
-        binding.digit1.setOnClickListener((v) -> insertSymbol("1"));
-        binding.digit2.setOnClickListener((v) -> insertSymbol("2"));
-        binding.digit3.setOnClickListener((v) -> insertSymbol("3"));
-        binding.digit4.setOnClickListener((v) -> insertSymbol("4"));
-        binding.digit5.setOnClickListener((v) -> insertSymbol("5"));
-        binding.digit6.setOnClickListener((v) -> insertSymbol("6"));
-        binding.digit7.setOnClickListener((v) -> insertSymbol("7"));
-        binding.digit8.setOnClickListener((v) -> insertSymbol("8"));
-        binding.digit9.setOnClickListener((v) -> insertSymbol("9"));
-        binding.digit0.setOnClickListener((v) -> insertSymbol("0"));
-        binding.dot.setOnClickListener((v) -> insertSymbol("."));
-        binding.leftBracket.setOnClickListener((v) -> insertSymbol("("));
-        binding.rightBracket.setOnClickListener((v) -> insertSymbol(")"));
-        binding.sum.setOnClickListener((v) -> insertSymbol("+"));
-        binding.subtraction.setOnClickListener((v) -> insertSymbol("-"));
-        binding.multiplication.setOnClickListener((v) -> insertSymbol("×"));
-        binding.division.setOnClickListener((v) -> insertSymbol("÷"));
+        binding.digit0.setOnClickListener((v) -> insertSymbol(((Button) v).getText()));
+        binding.digit1.setOnClickListener((v) -> insertSymbol(((Button) v).getText()));
+        binding.digit2.setOnClickListener((v) -> insertSymbol(((Button) v).getText()));
+        binding.digit3.setOnClickListener((v) -> insertSymbol(((Button) v).getText()));
+        binding.digit4.setOnClickListener((v) -> insertSymbol(((Button) v).getText()));
+        binding.digit5.setOnClickListener((v) -> insertSymbol(((Button) v).getText()));
+        binding.digit6.setOnClickListener((v) -> insertSymbol(((Button) v).getText()));
+        binding.digit7.setOnClickListener((v) -> insertSymbol(((Button) v).getText()));
+        binding.digit8.setOnClickListener((v) -> insertSymbol(((Button) v).getText()));
+        binding.digit9.setOnClickListener((v) -> insertSymbol(((Button) v).getText()));
+        binding.digit0.setOnClickListener((v) -> insertSymbol(((Button) v).getText()));
+        binding.dot.setOnClickListener((v) -> insertSymbol(((Button) v).getText()));
+        binding.leftBracket.setOnClickListener((v) -> insertSymbol(((Button) v).getText()));
+        binding.rightBracket.setOnClickListener((v) -> insertSymbol(((Button) v).getText()));
+        binding.sum.setOnClickListener((v) -> insertSymbol(((Button) v).getText()));
+        binding.subtraction.setOnClickListener((v) -> insertSymbol(((Button) v).getText()));
+        binding.multiplication.setOnClickListener((v) -> insertSymbol(((Button) v).getText()));
+        binding.division.setOnClickListener((v) -> insertSymbol(((Button) v).getText()));
         binding.clear.setOnClickListener((v) -> {
             int position = binding.input.getSelectionStart();
             if (position > 0) {
@@ -70,7 +71,7 @@ public class MainActivity extends AppCompatActivity {
         binding.exp.setOnClickListener((v) -> insertSymbol("e"));
     }
 
-    private void insertSymbol(String symbol) {
+    private void insertSymbol(CharSequence symbol) {
         int position = binding.input.getSelectionStart();
         Editable editable = binding.input.getText();
         editable.insert(position, symbol);
