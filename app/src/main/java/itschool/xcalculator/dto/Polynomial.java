9a2ca0fb8b77e57898325bcd63d92e482f8f5c7c
s0ba4 <sobachippin@gmail.com>
2021-05-22 20:06:24 +0200

    polynomial operations (sum, substraction, multiplication) added

--- app/src/main/java/itschool/xcalculator/dto/Polynomial.java
+++ app/src/main/java/itschool/xcalculator/dto/Polynomial.java
@@ -1,45 +1,89 @@
 package itschool.xcalculator.dto;
 
+import androidx.annotation.NonNull;
+
 import java.util.Arrays;
+import java.util.Objects;
 import java.util.function.BiFunction;
 
 public class Polynomial {
-    private final Double[] coeffs;
+    private final double[] coeffs;
 
-    public Polynomial(Double... coeffs) {
+    public Polynomial(double... coeffs) {
         this.coeffs = coeffs;
     }
 
+    public Polynomial(int... coeffs) {
+        final double[] doubles = new double[coeffs.length];
+        for (int i = 0; i < coeffs.length; i++) doubles[i] = coeffs[i];
+        this.coeffs = doubles;
+    }
+
     public Polynomial plus(Polynomial other) {
-        if (coeffs.length > other.coeffs.length) {
-            return doOperation(other.coeffs, coeffs, (n1, n2) -> n1 + n2);
-        } else {
-            return doOperation(coeffs, other.coeffs, (n1, n2) -> n1 + n2);
-        }
+        return (other.coeffs.length > coeffs.length)
+                ? doOperation(coeffs, other.coeffs, Double::sum)
+                : doOperation(other.coeffs, coeffs, Double::sum);
     }
 
     public Polynomial minus(Polynomial other) {
-        if (coeffs.length > other.coeffs.length) {
-            return doOperation(other.coeffs, coeffs, (n1, n2) -> n2 - n1);
-        } else {
-            return doOperation(coeffs, other.coeffs, (n1, n2) -> n2 - n1);
+        return (other.coeffs.length > coeffs.length)
+                ? doOperation(coeffs, other.coeffs, (a, b) -> a - b)
+                : doOperation(other.coeffs, coeffs, (a, b) -> a - b);
+    }
+
+    public Polynomial multiply(Polynomial other) {
+        Polynomial polynomial = null;
+        for (int i = 0; i < coeffs.length; i++) {
+            final double[] newCoeffs = new double[i + other.coeffs.length];
+            Arrays.fill(newCoeffs, 0.0);
+            for (int j = i; j < i + other.coeffs.length; j++) {
+                newCoeffs[j] = coeffs[i] * other.coeffs[j - i];
+            }
+            polynomial = (polynomial == null)
+                    ? new Polynomial(newCoeffs)
+                    : polynomial.plus(new Polynomial(newCoeffs));
         }
+        return Objects.requireNonNull(polynomial);
+    }
+
+    public int getOrder() {
+        return coeffs.length - 1;
     }
 
     private Polynomial doOperation(
-            Double[] shortest,
-            Double[] longest,
+            double[] shortest,
+            double[] longest,
             BiFunction<Double, Double, Double> function
     ) {
-        Double[] result = new Double[longest.length];
+        double[] result = new double[longest.length];
         for (int i = 0; i < shortest.length; i++) {
             result[i] = function.apply(shortest[i], longest[i]);
         }
         for (int i = shortest.length; i < longest.length; i++) {
-            result[i] = longest[i];
+            result[i] = function.apply(0.0, longest[i]);
         }
         return new Polynomial(result);
     }
+
+    @NonNull
+    @Override
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < coeffs.length; i++) {
+            final double coeff = coeffs[i];
+            if (coeff == 0.0) continue;
+            if (coeff > 0) builder.append('+');
+            if (i == 0) {
+                builder.append(coeff);
+            } else if (i == 1) {
+                builder.append(coeff).append('x');
+            } else {
+                builder.append(coeff).append("x^").append(i);
+            }
+        }
+        return String.format("P(%s)", builder.toString());
+    }
+
 
     @Override
     public boolean equals(Object o) {
--- app/src/test/java/itschool/xcalculator/PolynomialTest.java
+++ app/src/test/java/itschool/xcalculator/PolynomialTest.java
@@ -15,10 +15,17 @@ public class PolynomialTest {
     }
     @Test
     public void testMinus() {
-        Polynomial p1 = new Polynomial(1.0, 2.0, 3.0, 7.0);
-        Polynomial p2 = new Polynomial(4.0, 5.0, 6.0);
-        Polynomial p3 = new Polynomial(-3.0, -3.0, -3.0, 7.0);
+        Polynomial p1 = new Polynomial(1.0, 2.0);
+        Polynomial p2 = new Polynomial(1.0, 4.0, 6.0, 9.0);
+        Polynomial p3 = new Polynomial(0.0, -2.0, -6.0, -9.0);
         Assertions.assertEquals(p1.minus(p2), p3);
+    }
+    @Test
+    public void testMultiply() {
+        Polynomial p1 = new Polynomial(1.0, 2.0);
+        Polynomial p2 = new Polynomial(0.0, 1.0, 1.0);
+        Polynomial p3 = new Polynomial(0.0, 1.0, 3.0, 2.0);
+        Assertions.assertEquals(p1.multiply(p2), p3);
     }
 
 }
