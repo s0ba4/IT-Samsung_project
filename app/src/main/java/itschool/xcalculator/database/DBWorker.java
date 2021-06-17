352da4ca4198019778b5439315329ee743431bc0
s0ba4 <sobachippin@gmail.com>
2021-06-17 16:38:49 +0200

    leave only last 15 records

--- app/src/main/java/itschool/xcalculator/database/DBWorker.java
+++ app/src/main/java/itschool/xcalculator/database/DBWorker.java
@@ -34,8 +34,13 @@ public class DBWorker {
     public void saveHistoryItemAsync(HistoryItem item){
         final String expr = item.getExpression();
         final String ans = item.getAnswer();
-        Thread worker = new Thread(() -> getDB()
-                .fetch("insert into history(expr, ans) values ('" + expr + "','" + ans + "');"));
+        Thread worker = new Thread(() -> {
+            // добавление нового элемента в историю
+            getDB().fetch("insert into history(expr, ans) values ('" + expr + "','" + ans + "');");
+            // удаление всех старых элементов с сохранением последних 15 записей
+            // решение взято с: https://stackoverflow.com/questions/3667085/delete-oldest-records-from-database
+            getDB().fetch("delete from history where _id in (select _id from history order by _id desc limit -1 offset 15);");
+        });
         worker.setDaemon(true);
         worker.start();
     }
